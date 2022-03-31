e5aa0c438a03b71e62e186afcdbb8a98456d0e21
mingi3442 <mingi3442@gmail.com>
2022-04-01 03:48:18 +0900

    modify DataList

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -1,21 +1,15 @@
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const DataList = ({ seq, title, writer, createdAt, views }) => {
-  const history = useHistory();
-
-  const goView = () => {
-    history.push(`/view/${seq}`);
-  };
-
   return (
     <div className="board__list--container">
       <div className="board__list">
         <div className="board__list--cotents">
           <div className="board__list--num">{seq}</div>
-
-          <Link to={`/view/${seq}`}>{title}</Link>
-
+          <div className="board__list--title">
+            <Link to={`/view/${seq}`}>{title}</Link>
+          </div>
           <div className="board__list--writer">{writer}</div>
           <div className="board__list--date">{new Date(createdAt).toLocaleDateString()}</div>
           <div className="board__list--date">{views}</div>
