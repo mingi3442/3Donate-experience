715a610de67df38afcc10e32b21e1375f412c3de
mingi3442 <mingi3442@gmail.com>
2022-04-01 03:44:16 +0900

    modify

--- src/components/DataList.js
+++ src/components/DataList.js
@@ -13,13 +13,11 @@ const DataList = ({ seq, title, writer, createdAt, views }) => {
       <div className="board__list">
         <div className="board__list--cotents">
           <div className="board__list--num">{seq}</div>
-          <div className="board__list--title" onClick={goView}>
-            <Link to="/view/:seq">{title}</Link>
-          </div>
+
+          <Link to={`/view/${seq}`}>{title}</Link>
+
           <div className="board__list--writer">{writer}</div>
-          <div className="board__list--date">
-            {new Date(createdAt).toLocaleDateString()}
-          </div>
+          <div className="board__list--date">{new Date(createdAt).toLocaleDateString()}</div>
           <div className="board__list--date">{views}</div>
         </div>
       </div>
